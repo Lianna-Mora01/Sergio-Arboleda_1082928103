e35d049c0fab834bb67dd8852ca4a794be3d829d
Lianna Mora <lianna.mora01@usa.edu.co>
2026-03-06 07:38:00 -0500

    Tablas de multiplicar

---
+++ tablamultiplicar.py
@@ -0,0 +1,7 @@
+
+for i in range(1, 11):  
+    print(f"Tabla del {i}:")
+    for j in range(1, 11):  
+        resultado = i * j
+        print(f"{i} x {j} = {resultado}")
+    print() 
--- taller_1082928103.py
+++ taller_1082928103.py
@@ -7,5 +7,26 @@ for i in range(5):
     print("Hola Mundo")
 contador=1
 while contador<=5:
-    print("Númmero:" + str(contador))
-    contador = contador + 1
+ print("Númmero:" + str(contador))
+ contador = contador + 1
+
+ for i in range(5):
+        print("Número:" + str(i))
+
+for i in range(0,12, 6):
+        print("Número:" + str(i))
+
+ 
+
+enconrtado=False
+numerobuscado=7
+numeros =[1,3,5,7,9]
+for numero in numeros:
+        if numero==numerobuscado:
+         encontrado=True
+         break
+print ("Número", numerobuscado, "encontrado:", encontrado)
+
+for i in range(3):
+    for j in range(3):
+         print("i=" + str(i) + ", j=" + str(j))
