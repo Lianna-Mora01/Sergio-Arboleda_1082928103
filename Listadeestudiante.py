3c672ed51a8ccb9edd0f9a770029bfd760b57a4a
Lianna Mora <lianna.mora01@usa.edu.co>
2026-03-20 07:15:39 -0500

    Lista de estudiantes

---
+++ Listadeestudiante.py
@@ -0,0 +1,21 @@
+estudiantes = ["Laura", "Valerie" , "Juan" , "Shayla" , "Valeri" , "Adriana", "Marcos" , "Xavi" , "Juan orozco" , "María"]  
+estudiantes.append("Shayla")
+print(estudiantes) # ['Laura', 'Valerie', 'Juan', 'Shayla', 'Valeri', 'Adriana', 'Marcos', 'Xavi', 'Juan orozco', 'María', 'Shayla']
+
+#Cantidad de estudiantes
+print(len(estudiantes)) # 11
+
+# Buscar un estudiante
+if "Xavi" in estudiantes:
+    print("Xavi está en la lista de estudiantes.")
+if "Shayla" in estudiantes:
+    print("Shayla está en la lista de estudiantes.")
+if "Valeri" in estudiantes:
+    print("Valeri está en la lista de estudiantes.")
+if "Adriana" in estudiantes:
+    print("Adriana está en la lista de estudiantes.")
+ # Eliminar un estudiante
+estudiantes.remove("Juan orozco")
+print(estudiantes) # ['Laura', 'Valerie', 'Juan', 'Shayla', 'Valeri', 'Adriana', 'Marcos', 'Xavi', 'María', 'Shayla']
+estudiantes.remove("Shayla")
+print(estudiantes) # ['Laura', 'Valerie', 'Juan', 'Valeri', 'Adriana', 'Marcos', 'Xavi', 'María', 'Shayla']
